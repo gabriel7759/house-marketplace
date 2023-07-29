90bf88df03d52ebc9112780a23e6e3a0e8f0f5ce
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-07-29 15:25:23 -0700

    Enable registration a sign in with Google

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import Oauth from "../components/Oauth";
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -88,6 +89,8 @@ function SignIn() {
               </button>
             </div>
           </form>
+
+          <Oauth />
 
           <Link to="/sign-up" className="registerLink">
             Sign Up Instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,6 +11,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import Oauth from "../components/Oauth";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -115,6 +116,8 @@ function SignUp() {
               </button>
             </div>
           </form>
+
+          <Oauth />
 
           <Link to="/sign-in" className="registerLink">
             Sign In Instead
