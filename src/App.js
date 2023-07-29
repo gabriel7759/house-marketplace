ecd8408570b606f9e7691dc954ca4f0bdbcf8722
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-07-29 14:42:23 -0700

    Crearte a Private Rpoute Checker and a Spinner componet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
@@ -9,22 +10,23 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassowrd from "./pages/ForgotPassowrd";
 
-
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassowrd />} />
-      </Routes>
-      <NavBar />
-    </Router>
-    <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassowrd />} />
+        </Routes>
+        <NavBar />
+      </Router>
+      <ToastContainer />
     </>
   );
 }
