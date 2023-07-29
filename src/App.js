05ece6c557348a48b9e751c24be17b423ed94a02
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-07-29 11:36:08 -0700

    Implement Toastify when sign-in error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import NavBar from "./components/NavBar";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
@@ -22,6 +24,7 @@ function App() {
       </Routes>
       <NavBar />
     </Router>
+    <ToastContainer />
     </>
   );
 }
