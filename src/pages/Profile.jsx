158ebe5ac34a2c06f9f9e93548eedf82e5e8fdd7
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-07-29 12:00:18 -0700

    Happy path Logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
 
-function Profile()
-{
-  const [user, setUser] = useState(null);
-
+function Profile() {
   const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const { name, email} = formData;
 
-  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
